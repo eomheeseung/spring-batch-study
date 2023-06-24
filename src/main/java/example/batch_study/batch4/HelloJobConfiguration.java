bd0181c4a2204ebd10f455f552203ca696d3b4fa
eomheeseung <jeaimeshe@gamil.com>
2023-06-24 17:36:07 +0900

    batch4

---
+++ src/main/java/example/batch_study/batch4/HelloJobConfiguration.java
@@ -0,0 +1,117 @@
+package example.batch_study.batch4;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
+import org.springframework.batch.core.step.job.JobParametersExtractor;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+@Slf4j
+public class HelloJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("job1")
+                .start(step1())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new CustomTasklet())
+                .build();
+
+    }
+
+    /**
+     * startLimit() 사용
+     */
+   /* @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new CustomTasklet())
+                .startLimit(3)// step1은 3번만 실행이 가능하다.
+                .build();
+    }*/
+
+    /**
+     * allowStartIfComplete() 사용
+     */
+    /*@Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new CustomTasklet())
+                .allowStartIfComplete(true) // job이 실행될때마다 무조건 실행된다.
+                .build();
+    }*/
+
+    @Bean
+    public Job helloJob2() {
+        return jobBuilderFactory.get("parentJob")
+                .start(jobStep(null))
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step jobStep(JobLauncher jobLauncher) {
+        return stepBuilderFactory.get("jobStep")
+                .job(childJob())
+                .launcher(jobLauncher)
+                .parametersExtractor(jobParametersExtractor())
+                // 리스너를 통해서 step이 시작되기 전에
+                // step의 ExecutionContext에 key, value값 등록
+                .listener(new StepExecutionListener() {
+                    @Override
+                    public void beforeStep(StepExecution stepExecution) {
+                        stepExecution.getExecutionContext().putString("name", "heeseung");
+                    }
+
+                    @Override
+                    public ExitStatus afterStep(StepExecution stepExecution) {
+                        return null;
+                    }
+                }).build();
+    }
+
+    // step의 Executioncontext에 name의 key를 갖는 key:value를 꺼내서
+    // JobStep의 JobParameters로 만들어서 넘긴다.
+    private JobParametersExtractor jobParametersExtractor() {
+        DefaultJobParametersExtractor extractor = new DefaultJobParametersExtractor();
+        extractor.setKeys(new String[]{"name"});
+        return extractor;
+    }
+
+    @Bean
+    public Job childJob() {
+        return jobBuilderFactory.get("childJob")
+                .start(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((stepContribution, chunkContext) -> {
+                    JobParameters jobParameters = stepContribution.getStepExecution().getJobParameters();
+                    System.out.println(jobParameters.getString("name"));
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+
+}
